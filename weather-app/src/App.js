0e5024ecc2d9532a3f3285b8a77759661def6d40
TamimFatema <1910807@iub.edu.bd>
2023-09-27 14:34:30 +0600

    react installation

---
+++ weather-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="md max-w-screen mt-4  py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+
+      </div>
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ weather-app/src/Components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { UilSearchAlt } from '@iconscout/react-unicons'
+import { UilLocationArrow } from '@iconscout/react-unicons'
+function Search() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Search
